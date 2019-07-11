0dc4183c453d63e9c60fb455ecbe5807a0395a91
Valerie Bragg <vbragg@ufledu>
2019-07-11 15:25:44 -0400

    Add variables and event listener to JavaScript file

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 // Global Variables
-
+const myBoxes = document.getElementsByClassName('block');
 
 // The Functionality
+function loopThrough() {
+  
+};
 
 
 // The Event Listener
+for (let i=0; i < myBoxes.length; i++) {
+  myBoxes.addEventListener('click', loopThrough);
+};
