c030d72db7dc99cc090984f92f8b75a051291f74
Valerie Bragg <vbragg@ufledu>
2019-07-11 16:09:30 -0400

    Add teal background color for flash and fix JavaSCript errors

--- main.js
+++ main.js
@@ -4,15 +4,16 @@ const myBoxes = document.getElementsByClassName('block');
 // The Functionality
 function loopThrough() {
 
-  const changeColor = () => this.classList.toggle('red');
+  const changeColor = () => this.classList.toggle('teal');
 
-  for (let i=0; i <0; i < this.textContent.length; i++) {
+  for(let i = 0; i < this.textContent.length * 2; i++) {
+    setTimeout(changeColor, i * 1000);
+  };
 
-    }
 };
 
 
 // The Event Listener
-for (let i=0; i < myBoxes.length; i++) {
-  myBoxes.addEventListener('click', loopThrough);
+for (let i = 0; i < myBoxes.length; i++) {
+  myBoxes[i].addEventListener('click', loopThrough);
 };
